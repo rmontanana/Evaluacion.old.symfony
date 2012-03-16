abf2aeed3f9045fa56e7d1ef405d701689af25b4
Ricardo Montañana Gómez <rmontanana@gmail.com>
2012-03-16 02:17:14 +0100

    *Creada clase Util para generar menú de momento a partir de menu.yml *Creado menu.yml con el menú de la aplicación. *Creado layout.html.twig para contener la configuración de aspecto del bundle. *Modificado el controlador Default para comprobar el funcionamiento del menú.

--- src/Evaluacion/AppBundle/Controller/DefaultController.php
+++ src/Evaluacion/AppBundle/Controller/DefaultController.php
@@ -3,13 +3,14 @@
 namespace Evaluacion\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Evaluacion\AppBundle\Util\Util;
 
 
 class DefaultController extends Controller
 {
-    
     public function indexAction($name)
     {
-        return $this->render('AppBundle:Default:index.html.twig', array('name' => $name));
+        $menu = Util::getMenu();
+        return $this->render('AppBundle:Default:index.html.twig', array('name' => $name, 'menu' => $menu));
     }
 }
---
+++ src/Evaluacion/AppBundle/Util/Util.php
@@ -0,0 +1,46 @@
+<?php
+/* ***************************************************************************
+ * AppBundle/Util/Util.php
+ * Clase Util donde realizan diversas acciones
+ * (C) Copyright 2012 Ricardo Montañana <rmontanana@gmail.com>
+ * This file is part of Evaluacion.
+ * ***************************************************************************
+ * Evaluacion is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Evaluacion is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Evaluacion (LICENSE file). 
+ * If not, see <http://www.gnu.org/licenses/>
+ * ***************************************************************************
+ */   
+
+
+namespace Evaluacion\AppBundle\Util;
+
+use Symfony\Component\Yaml\Parser;
+
+
+Class Util
+{
+    /**
+     * Devuelve el menú de la aplicación
+     * @return string
+     */
+    static public function getMenu()
+    {
+        // Carga el fichero de configuración donde reside el menú
+        $configuracion = __DIR__ ."/../Resources/config/menu.yml";
+        $yaml = new Parser();
+        // Llama al parser de symfony para que devuelva un array con el menú
+        $menu = $yaml->parse(file_get_contents($configuracion));
+        return $menu;
+    }
+}
+
